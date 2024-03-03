4aab7fc488a16fc87877471b3bae044ff7c33fe8
demilade211 <afolabiogunbanwo@gmail.com>
2024-03-03 11:58:38 +0100

    added timestamps to withdrawal model

--- models/user.js
+++ models/user.js
@@ -9,19 +9,19 @@ const UserSchema = new Schema({
         type: String,
         maxlength: [30, "Your name cannot exceed 30 characters"]
 
-    }, 
+    },
     email: {
         type: String,
         required: [true, 'Please enter your email'],
         unique: true,// cant have more than one if this
         validate: [validator.isEmail, 'Please enter valid email address']
-    }, 
+    },
     password: {
         type: String,
         required: [true, 'Please enter your password'],
         minlength: [6, 'Your password must be longer than 6 characters'],
         select: false
-    }, 
+    },
     verified: {
         type: Boolean,
         default: false
@@ -29,15 +29,15 @@ const UserSchema = new Schema({
     isDeactivated: {
         type: Boolean,
         default: false
-    }, 
+    },
     role: {
         type: String,
         default: "user",
         enum: ['user', 'admin']// specifies the only values that will be in role
-    },  
+    },
     resettoken: { type: String },
     expiretoken: { type: Date },
-    authorizations:[]
+    authorizations: []
 },
     { timestamps: true });
 
--- models/withdrawal.js
+++ models/withdrawal.js
@@ -3,34 +3,35 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 
-const WithdrawalSchema = new Schema({ 
-    user:{
+const WithdrawalSchema = new Schema({
+    user: {
         type: Schema.Types.ObjectId,//gets the  _id from user model
         ref: "User",
         required: true,
     },
-    atmId:{
+    atmId: {
         type: Schema.Types.ObjectId,//gets the  _id from user model
         ref: "Atm",
         required: true,
     },
-    amount:{
+    amount: {
         type: Number,
-        required:true
-    },
-    withdrawal_status:{
-        type:String,
-        enum:['pending','successful','failed','wrong'],
-        required:true,
-    },
-    reference:{
-        type:String,
         required: true
     },
-    atmReference:{
-        type:String,
+    withdrawal_status: {
+        type: String,
+        enum: ['pending', 'successful', 'failed', 'wrong'],
+        required: true,
+    },
+    reference: {
+        type: String,
+        required: true
+    },
+    atmReference: {
+        type: String,
         required: true
     }
-})
+},
+    { timestamps: true });
 
 module.exports = mongoose.model("Withdrawal", WithdrawalSchema)
